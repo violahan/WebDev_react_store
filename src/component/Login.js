54eb08bc3dcc71858c65cd783e3f1ca82e35377f
Viola Han <hanjiaqi0122@gmail.com>
2021-02-22 20:08:59 +1300

    Use JSX to Write HTML

--- src/component/Login.js
+++ src/component/Login.js
@@ -2,7 +2,24 @@ import React from 'react';
 
 class Login extends React.Component{
     render(){
-        return <p>Login Component</p>
+        return <form className={"login-wrapper"}>
+           <div>
+               <label>Email</label>
+               <div>
+                   <input type={"text"} placeholder={"Email"}/>
+               </div>
+           </div>
+
+            <div>
+                <label>Password</label>
+                <div>
+                    <input type={"text"} placeholder={"Password"}/>
+                </div>
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
     }
 }
 
