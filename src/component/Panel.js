9addef6ffe9c40ee0ba9d90d9d2a209a5c042f8a
Viola Han <hanjiaqi0122@gmail.com>
2021-05-26 15:27:37 +1200

    Add Edit Inventory Function

--- src/component/Panel.js
+++ src/component/Panel.js
@@ -10,10 +10,20 @@ class Panel extends React.Component {
         }
     };
 
-    open = options => {
-        const {component, callback} = options;
+    open = (
+        options = {
+            props: {},
+            component: null,
+            callback: () => {}
+        }
+    )=> {
+        const {props, component, callback} = options;
         const _key = new Date().getTime()
-        const _component = React.createElement(component, {close: this.close, key: _key});
+        const _component = React.createElement(component, {
+            ...props,
+            close: this.close,
+            key: _key
+        });
         this.setState({
             active: true,
             component: _component,
--- src/component/Product.js
+++ src/component/Product.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import {formatPrice} from "../commons/helper";
+import Panel from "./Panel";
+import EditInventory from "./EditInventory";
 
 class Product extends React.Component{
+
+    toEdit = () => {
+        Panel.open({
+            component: EditInventory,
+            props:{
+                product: this.props.product,
+            },
+            callback: data =>{
+                if (data){
+                    this.props.update(data);
+                }
+            }
+        });
+    };
 
     render() {
         const { name, image, tags, price, status }= this.props.product
@@ -13,6 +29,11 @@ class Product extends React.Component{
         return(
             <div className={_pClass[status]}>
                 <div className={"p-content"}>
+                    <div className={"p-head has-text-right"} onClick={this.toEdit}>
+                        <span className={"icon edit-btn"}>
+                           <i className={"fas fa-sliders-h"}></i>
+                        </span>
+                    </div>
                     <div className={"img-wrapper"}>
                         <div className={"out-stock-text"}>Out Of Stock</div>
                         <figure className="image is-4by3 ">
