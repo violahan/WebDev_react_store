d706d9d2f478fd274868db4d7b02b03941dd4caa
Viola Han <hanjiaqi0122@gmail.com>
2021-07-28 15:53:17 +1200

    Update Cart's API Authority

--- src/commons/axios.js
+++ src/commons/axios.js
@@ -1,12 +1,27 @@
 import _axios from 'axios'
 
-const  axios = baseURL => {
+const axios = baseURL => {
     const instance = _axios.create({
-        baseURL: baseURL || 'http://localhost:3003',
-        timeout: 1000,
+        baseURL:
+            baseURL || process.env.REACT_APP_API_DOMAIN || 'http://localhost:3003',
+        timeout: 1000
     });
+
+    instance.interceptors.request.use(
+        config => {
+            const jwToken = global.auth.getToken();
+            config.headers['Authorization'] = 'Bearer ' + jwToken;
+            // Do something before request is sent
+            return config;
+        },
+        error => {
+            // Do something with request error
+            return Promise.reject(error);
+        }
+    );
+
     return instance;
-}
+};
 
 export {axios};
 
