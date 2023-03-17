f40e88411fd3bae133eff6b6654cefc205101370
Viola Han <hanjiaqi0122@gmail.com>
2023-03-17 16:33:31 +1300

    Define Models

---
+++ server/db/model.js
@@ -0,0 +1,39 @@
+// Requiring module
+const mongoose = require('mongoose');
+
+const usersSchema = new mongoose.Schema({
+    id: Number,
+    email: String,
+    nickname: String,
+    password: String,
+    type: Number
+});
+
+const productsSchema = new mongoose.Schema({
+    id: Number,
+    name: String,
+    image: String,
+    tags: String,
+    price: String,
+    status: String
+});
+
+const cartSchema = new mongoose.Schema({
+    productId: Number,
+    name: String,
+    image: String,
+    price: Number,
+    mount: Number,
+    userId: String,
+    id: Number
+})
+
+// Creating model objects
+const User = mongoose.model('users', usersSchema);
+const Product = mongoose.model('products', productsSchema);
+const Cart = mongoose.model('carts', cartSchema);
+
+// Exporting our model objects
+module.exports = {
+    User, Product, Cart
+}
