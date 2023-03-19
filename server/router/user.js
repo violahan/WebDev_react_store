12ac27a8829e67cad876bbdcbe039d655c91f2d8
Viola Han <hanjiaqi0122@gmail.com>
2023-03-19 19:30:12 +1300

    Create APIs for retrieving and adding products by user ID

--- server/router/user.js
+++ server/router/user.js
@@ -1,7 +1,7 @@
 const express = require ('express')
 const userRouter = express.Router()
 
-const { User } = require('../db/model');
+const { User, Product} = require('../db/model');
 
 userRouter.get("/users", async (req, res) => {
     try{
@@ -28,6 +28,46 @@ userRouter.post('/users', async (req, res) => {
     }
     catch (error) {
         res.status(400).json({message: error.message})
+    }
+})
+
+
+userRouter.get('/users/:id', async (req, res) => {
+    try{
+        const data = await User.findById(req.params.id);
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+
+userRouter.put('/users/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+        const options = { new: true };
+
+        const result = await User.findByIdAndUpdate(
+            id, updatedData, options
+        )
+
+        res.send(result)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
+userRouter.delete('/users/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const data = await User.findByIdAndDelete(id)
+        res.send(`Document with ${data.nickname} has been deleted..`)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
     }
 })
 
