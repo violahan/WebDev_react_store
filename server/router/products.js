6e2e643f2188225464bc8530a5427f1046620d07
Viola Han <hanjiaqi0122@gmail.com>
2023-03-19 19:18:27 +1300

    Create APIs for retrieving and adding products by ID

--- server/router/products.js
+++ server/router/products.js
@@ -32,4 +32,44 @@ productRouter.post('/products', async (req, res) => {
     }
 })
 
+productRouter.get('/products/:id', async (req, res) => {
+    try{
+        const data = await Product.findById(req.params.id);
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+
+productRouter.put('/products/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+        const options = { new: true };
+
+        const result = await Product.findByIdAndUpdate(
+            id, updatedData, options
+        )
+
+        res.send(result)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
+productRouter.delete('/products/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const data = await Product.findByIdAndDelete(id)
+        res.send(`Document with ${data.name} has been deleted..`)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
+
 module.exports = productRouter
